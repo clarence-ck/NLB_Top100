84c97d96f5e2ab70453d243939df010fdce8c12a
Clarence Sai <82635407+clarence-ck@users.noreply.github.com>
2024-10-31 16:49:30 +0800

    Changes to move excel to src, rename iron_viz_nlb to dev

--- src/app.py
+++ src/app.py
@@ -6,10 +6,9 @@ import plotly.graph_objects as go
 from datetime import datetime
 
 # Load the dataset
-file_path = "C:\\Users\\Clarence\\clarence-ck\\GitHub\\NLB_Top100\\Top_100_OD_Titles_CY2020_to_2023.xlsx"
+file_path = './Top_100_OD_Titles_CY2020_to_2023.xlsx'
 data = pd.read_excel(file_path, sheet_name='Sheet1')
 
-# Data Preprocessing
 # Convert 'Title Publication Date' to datetime
 data['Title Publication Date'] = pd.to_datetime(data['Title Publication Date'], format='%Y-%m-%d')
 data['Publication Year'] = data['Title Publication Date'].dt.year
